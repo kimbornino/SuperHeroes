9f9a1eca92c937e30ca3cce539d6eda33382e38e
Kimbornino <kimbornino@gmail.com>
2018-09-27 13:31:05 -0500

    Added a controller for Heroes.

---
+++ SuperHeroes/SuperHeroes/Controllers/HeroesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class HeroesController : Controller
+    {
+        // GET: Heroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
