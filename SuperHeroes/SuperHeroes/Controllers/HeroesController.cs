e370f54f41fced46a5ba22815200ddc794cf6965
Kimbornino <kimbornino@gmail.com>
2018-09-27 15:16:15 -0500

    Added a view called Index and changed name of old view to hero.

--- SuperHeroes/SuperHeroes/Controllers/HeroesController.cs
+++ SuperHeroes/SuperHeroes/Controllers/HeroesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperHeroes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace SuperHeroes.Controllers
 {
     public class HeroesController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+        //public Heroes GetHeroById(int id)
+        //{
+        //    Heroes hero;
+        //    // query for hero by ID
+        //    return hero;
+        //}
         // GET: Heroes
         public ActionResult Index()
         {
-            return View();
+            //could add . where after Heroes to narrow search
+            var heroes = db.Heroes;
+            return View(heroes);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] Heroes hero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Heroes.Add(hero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            //ViewBag.Name = new SelectList(db.Heroes, "ID", "Name", hero.Name);
+            return View(hero);
         }
     }
 }
